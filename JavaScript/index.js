ff10f2773e16cd3fa6e2799929df52f498c27f69
BerkanBal <bberkanbal@hotmail.com>
2025-07-10 23:56:04 +0300

    sepet kısmı eklendi

---
+++ JavaScript/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const sepet = document.querySelector('.cart-icon');
+  const offScreenSepet = document.querySelector('.off-screen-sepet');
+
+  sepet.addEventListener('click', () => {
+    sepet.classList.toggle('active');
+    offScreenSepet.classList.toggle('active');
+  });
+});
