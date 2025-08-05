f5296fae9be28de0c0f1e47a064bd931f470c585
BerkanBal <bberkanbal@hotmail.com>
2025-08-05 17:49:22 +0300

    Urunler sayfasi urunleri databaseden cekildi

--- Routes/user.js
+++ Routes/user.js
@@ -1,6 +1,30 @@
 const express =  require("express");
 const router = express.Router();
 const path = require("path");
+const db = require("../data/db")
+
+
+router.get("/urunler", async function(req,res){
+    try {
+        const [urunler] = await db.execute("SELECT *FROM urunler");
+        const [hamburgerler] = await db.execute("SELECT *FROM urunler Where kategori = '1' ");
+        const [kizartmalar] = await db.execute("SELECT *FROM urunler Where kategori = '2' ");
+        const [icecekler] = await db.execute("SELECT *FROM urunler Where kategori = '3' ");
+
+        res.render("users",{
+            urunler: urunler ,
+            hamburgerler: hamburgerler ,
+            kizartmalar: kizartmalar ,
+            icecekler: icecekler
+        });
+
+    } 
+    
+    catch (err) {
+        console.log(err);
+        res.status(500).send("Sunucu Hatasi");
+    }
+});
 
 router.use("/urunler", function(req,res){
     res.render("users");
